64af2de73c7b33ffa2134065ba530e970dcc71cd
erenyywadie <hc8224jhfj@privaterelay.appleid.com>
2026-08-17 21:24:06 +0300

    Add protected route to auth routes
    Added a protected route for testing access control.

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { signup, login } = require("../controllers/auth-controller");
+const { signup, login, protect } = require("../controllers/auth-controller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/protected-test", protect, (req, res) => {
+  res.status(200).json({ message: "You accessed a protected route!", user: req.user });
+});
 
 module.exports = router;
